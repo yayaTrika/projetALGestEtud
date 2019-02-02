ce021dcff955301fd64ef0b7a08ad6303959a8e6
yaya <yayatrika03@yahoo.fr>
2019-02-02 12:05:29 +0000

    create model, factory, migration, controller and, resource student and level

---
+++ app/Http/Controllers/NiveauController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Model\Niveau;
+use Illuminate\Http\Request;
+
+class NiveauController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Model\Niveau  $niveau
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Niveau $niveau)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Model\Niveau  $niveau
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Niveau $niveau)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Model\Niveau  $niveau
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Niveau $niveau)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Model\Niveau  $niveau
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Niveau $niveau)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,6 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource('/students','EtudiantController');
+Route::apiResource('/levels','NiveauController');
